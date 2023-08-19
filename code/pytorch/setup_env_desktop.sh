f0bb17ae13c2d4817c3edf07572448b853b027b6
Eric Phan <ephan@nvidia.com>
2023-08-19 16:18:26 -0700

    convert to workbench

---
+++ code/pytorch/setup_env_desktop.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+sudo apt -qq install -y sox
+
+python3.7 -m pip install --upgrade pip
+python3.7 -m pip install cython
+python3.7 -m pip install nvidia-pyindex
+python3.7 -m pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+
+mkdir content 
+cd content
+git clone https://github.com/NVIDIA/NeMo.git
+cd NeMo
+git reset --hard e856e9732af79a6ed4bffaa3d709bfa387799587
+sudo cp PATH_TO_COLAB_NOTEBOOKS/pytorch/nemo_nlp_model.patch /opt/nemo_nlp_model.patch
+git apply /opt/nemo_nlp_model.patch
+./reinstall.sh
+sudo rm -rf /opt/nemo_nlp_model.patch
+python3.7 -m pip uninstall tokenizers -y
+
+# Install Cmake
+sudo mkdir -p /tmp_dir_cmake && sudo chmod -R 777 /tmp_dir_cmake
+cd /tmp_dir_cmake
+wget https://github.com/Kitware/CMake/releases/download/v3.14.4/cmake-3.14.4-Linux-x86_64.sh
+chmod +x cmake-3.14.4-Linux-x86_64.sh
+sudo ./cmake-3.14.4-Linux-x86_64.sh --prefix=/usr/local --exclude-subdir --skip-license
+rm ./cmake-3.14.4-Linux-x86_64.sh
+
+#Install tao whls
+python3.7 -m pip install https://files.pythonhosted.org/packages/0e/32/6761e35c533d854ce7e5e51908bac3ae1488d94c110949e467345b28334c/nvidia_eff_tao_encryption-0.1.6-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
+python3.7 -m pip install --extra-index-url https://developer.download.nvidia.com/compute/redist nvidia-eff==0.6.3
+python3.7 -m pip install nvidia-tao==4.0.0
+python3.7 -m pip install nvidia-tao-pytorch==4.0.1.dev0
+python3.7 -m pip install --ignore-installed --no-deps -r PATH_TO_COLAB_NOTEBOOKS/pytorch/requirements-pip.txt
+
+#Reset some packages to required versions
+python3.7 -m pip install pytorch_lightning==1.6.0
+python3.7 -m pip install transformers==4.8.2
+python3.7 -m pip install tokenizers==0.10.3
+python3.7 -m pip install huggingface-hub==0.4.0
+python3.7 -m pip install torchmetrics==0.10.3
